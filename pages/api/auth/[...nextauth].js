454fafa50bd4ab2a8bf27c193623723a18cb970c
Tri Vuong <trivektor@gmail.com>
2020-12-12 22:17:27 -0800

    Setup Google Oauth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,27 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+const options = {
+  providers: [
+    Providers.Google({
+      clientId: process.env.NEXT_TICKET_TRACKING_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_TICKET_TRACKING_GOOGLE_CLIENT_SECRET
+    }),
+  ],
+  callbacks: {
+    signIn: async (user, account, profile) => {
+      return Promise.resolve(true);
+    },
+    redirect: async (url, baseUrl) => {
+      return Promise.resolve(baseUrl);
+    },
+    session: async (session, user) => {
+      return Promise.resolve(session);
+    },
+    jwt: async (token, user, account, profile, isNewUser) => {
+      return Promise.resolve(token);
+    },
+  }
+}
+
+export default (req, res) => NextAuth(req, res, options)
